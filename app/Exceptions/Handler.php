0e55a58ffe9cad6875d00fc7e30d53fa67c66215
Aliff <aliff.rosli96@gmail.com>
2021-07-27 14:35:33 +0800

    Custom message if requested not available error added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -38,4 +39,20 @@ class Handler extends ExceptionHandler
             //
         });
     }
+/**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json(['message' => 'The requested resources is not available!'], 404);
+        }
+    }
+    
 }
