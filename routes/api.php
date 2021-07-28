12d7f2bc1165881bdda663bf58141b17b0e39b4d
Aliff <aliff.rosli96@gmail.com>
2021-07-28 22:21:41 +0800

    Route updated / add prefix

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', [App\Http\Controllers\API\UserController::class,'index']);
-Route::get('/users/show/{user}', [App\Http\Controllers\API\UserController::class,'show']);
-Route::post('/users/create', [App\Http\Controllers\API\UserController::class,'store']);
-Route::post('/users/update/{user}', [App\Http\Controllers\API\UserController::class,'update']);
-Route::delete('/users/{user}', [App\Http\Controllers\API\UserController::class,'delete']);
+Route::prefix('v1')->middleware('auth:api')->group(function(){
+        Route::get('/users', [App\Http\Controllers\API\UserController::class,'index']);
+        Route::get('/users/show/{user}', [App\Http\Controllers\API\UserController::class,'show']);
+        Route::post('/users/create', [App\Http\Controllers\API\UserController::class,'store']);
+        Route::post('/users/update/{user}', [App\Http\Controllers\API\UserController::class,'update']);
+        Route::delete('/users/{user}', [App\Http\Controllers\API\UserController::class,'delete']);
 
-Route::get('/inventory', [App\Http\Controllers\API\InventoryController::class,'index']);
-Route::post('/inventory/create', [App\Http\Controllers\API\InventoryController::class,'store']);
+        Route::get('/inventory', [App\Http\Controllers\API\InventoryController::class,'index']);
+        Route::get('/inventory/show/{inventories}', [App\Http\Controllers\API\InventoryController::class,'index']);
+        Route::post('/inventory/create', [App\Http\Controllers\API\InventoryController::class,'store']);
+        Route::post('/inventory/update/{inventories}', [App\Http\Controllers\API\InventoryController::class,'store']);
+        Route::delete('/inventory/{inventories}', [App\Http\Controllers\API\InventoryController::class,'store']);
+});
